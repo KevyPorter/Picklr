59cb4504d66e78e74fda6c66901a6ecfd9ae758d
twemyeez <thomas@thomaswemyss.com>
2014-09-19 15:52:57 +0100

    Add the initial base files

---
+++ src/main/java/com/twemyeez/picklr/Picklr.java
@@ -0,0 +1,14 @@
+package com.twemyeez.picklr;
+
+import cpw.mods.fml.common.Mod;
+
+
+
+@Mod(modid = Picklr.MODID, version = Picklr.VERSION)
+public class Picklr
+{
+    public static final String MODID = "Picklr";
+    public static final String VERSION = "Lambda";
+    
+ 
+}
