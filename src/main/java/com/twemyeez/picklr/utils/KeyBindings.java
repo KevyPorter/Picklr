4d7b2fc2e5c80f301bd82ff26539a58d1001666f
twemyeez <twemyeez@users.noreply.github.com>
2014-10-04 16:01:59 +0100

    Add lobby key

--- src/main/java/com/twemyeez/picklr/utils/KeyBindings.java
+++ src/main/java/com/twemyeez/picklr/utils/KeyBindings.java
@@ -1,7 +1,15 @@
 package com.twemyeez.picklr.utils;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.lwjgl.input.Keyboard;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.ListIterator;
+import com.twemyeez.picklr.Picklr;
 import com.twemyeez.picklr.config.ConfigurationHandler;
 import com.twemyeez.picklr.config.ConfigurationHandler.ConfigAttribute;
 import com.twemyeez.picklr.friends.Friend;
@@ -10,6 +18,7 @@ import com.twemyeez.picklr.location.ServerLocationUtils;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.EnumChatFormatting;
 import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
@@ -24,15 +33,23 @@ public class KeyBindings {
 	public static KeyBinding changeRightClickActionToAdd = new KeyBinding(
 			"Right click to add", Keyboard.KEY_O, "Picklr");
 
+	public static KeyBinding lobby = new KeyBinding("Lobby", Keyboard.KEY_L,
+			"Picklr");
+
+	public static List<KeyBinding> pressedWithinTimeframe = new ArrayList<KeyBinding>();
+
 	public KeyBindings() {
 		// Register the friend list key binding
 		ClientRegistry.registerKeyBinding(friendList);
 
 		// Register the server HUD key binding
 		ClientRegistry.registerKeyBinding(toggleLocationHud);
-		
-		//Register the right click add friend key
+
+		// Register the right click add friend key
 		ClientRegistry.registerKeyBinding(changeRightClickActionToAdd);
+
+		// Register key binding for lobby
+		ClientRegistry.registerKeyBinding(lobby);
 	}
 
 	@SubscribeEvent
@@ -87,6 +104,79 @@ public class KeyBindings {
 						ServerLocationUtils.locationHudEnabled);
 			}
 
+		}
+
+		if (lobby.isPressed()) {
+			// If it was the lobby key
+			
+			//Check it is Hypixel
+			if (!CommonUtils.isHypixel()) {
+				return;
+			}
+
+			//Check if the lobby button is enabled
+			if (!((Boolean) ConfigurationHandler
+					.getConfigurationAttribute(ConfigAttribute.LOBBY_ENABLED))) {
+				return;
+			}
+
+			// Otherwise, continue
+			if (pressedWithinTimeframe.contains(lobby)) {
+				// It has been pressed once within the timeframe so carry out
+				// the action
+				Minecraft.getMinecraft().thePlayer
+						.sendChatMessage("/lobby "
+								+ ConfigurationHandler
+										.getConfigurationAttribute(ConfigAttribute.DEFAULT_LOBBY));
+
+				// Alert the user
+				CommonUtils
+						.sendFormattedChat(
+								true,
+								EnumChatFormatting.GRAY
+										+ "["
+										+ EnumChatFormatting.LIGHT_PURPLE
+										+ "Lobby"
+										+ EnumChatFormatting.GRAY
+										+ "] Lobbied to "
+										+ ConfigurationHandler
+												.getConfigurationAttribute(ConfigAttribute.DEFAULT_LOBBY),
+								EnumChatFormatting.GOLD, true);
+
+				// Remove the keybinding from the timeout
+				Iterator<KeyBinding> iterator = pressedWithinTimeframe
+						.iterator();
+				// While there is more left to iterate, continue
+				while (iterator.hasNext()) {
+					// Check if it is the lobby keybinding
+					if (iterator.next().equals(lobby)) {
+						// If so, remove
+						iterator.remove();
+					}
+				}
+			} else {
+				// Add the keybinding to the list
+				pressedWithinTimeframe.add(lobby);
+				// Then, schedule a new timer to remove it after 1 second
+				new Timer().schedule(new TimerTask() {
+
+					@Override
+					public void run() {
+						// Remove the keybinding from the timeout
+						Iterator<KeyBinding> iterator = pressedWithinTimeframe
+								.iterator();
+						// While there is more left to iterate, continue
+						while (iterator.hasNext()) {
+							// Check if it is the lobby keybinding
+							if (iterator.next().equals(lobby)) {
+								// If so, remove
+								iterator.remove();
+							}
+						}
+					}
+
+				}, 1000);
+			}
 		}
 	}
 }
